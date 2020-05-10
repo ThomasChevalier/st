d5c4c34424e11d5c54208f42d9bf52d1d9dbaff9
ThomasChevalier <thomasom.chevalier@gmail.com>
2020-05-10 21:59:09 +0200

    Change font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode:style=SemiBold:pixelsize=12:antialias=true:autohint=true";
+static char *font = "FiraCode:style=Medium:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
